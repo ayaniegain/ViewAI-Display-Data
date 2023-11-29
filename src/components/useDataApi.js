e33e37bd087d5883cf027fa1835324de751d5156
ayaniegain <ayaniegain@gmail.com>
2023-11-29 15:54:45 +0530

    fix: code

--- src/components/useDataApi.js
+++ src/components/useDataApi.js
@@ -11,7 +11,7 @@ export function useDataApi() {
         setResults(response.results);
 
       } catch (error) {
-        console.log(error.message);
+        // console.log(error.message);
       }
     }
     fetchMyAPI();
